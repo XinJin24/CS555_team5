e5f594b7102d7ac2ac0f4e73457241995683b491
joemarks <jmichaelmarks@tutanota.com>
2022-10-16 15:01:31 -0400

    clean up test printing

--- GEDCOM_ParseCode.py
+++ GEDCOM_ParseCode.py
@@ -266,10 +266,6 @@ for key in familyDictionary:
                     ,familyDictionary.get(key).get('Children')])
 print(families)
 
-# for testing - to delete
-print(individualDictionary)
-print("\n")
-print(familyDictionary)
 
 
 
