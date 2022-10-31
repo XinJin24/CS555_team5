b9a1ce385a37f23b37f62380d9ca1b774e122206
Joseph Marks <87625909+JosephMarks@users.noreply.github.com>
2022-10-31 19:48:26 -0400

    Update test_sprint3_userStories.py

--- Sprint3/test_sprint3_userStories.py
+++ Sprint3/test_sprint3_userStories.py
@@ -171,6 +171,113 @@ class userStories17And18Test(unittest.TestCase):
                            'Wife_ID': 'I99', 'Wife_Name': 'Anna /Marks/', 'Children': []}
          result = siblingsShouldNotMarry(individualDictionary, familyDictionary)
          self.assertFalse(result)
+        
+class test_us19_no_marriage_to_first_cousin(unittest.TestCase):
+
+    def generate_Individual_Dictionary(self):
+        individualDictionary = {}
+        individualDictionary['I1'] = {'ID': 'I1', 'Name': 'Joseph /Marks/', 'Gender': 'M', 'Birthday': '1985-05-13', 'Age': 37, 'Alive': 'True', 'Death': 'NA', 'Child': 'F1', 'Spouse': []}
+        individualDictionary['I2'] = {'ID': 'I2', 'Name': 'Joyce /Earnhart/', 'Gender': 'F', 'Birthday': '1951-10-7', 'Age': 71, 'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F1']}
+        individualDictionary['I3'] = {'ID': 'I3', 'Name': 'Don /Marks/', 'Gender': 'M', 'Birthday': '1949-08-15', 'Age': 73, 'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F1']}
+        individualDictionary['I4'] = {'ID': 'I4', 'Name': 'Jacob /Marks/', 'Gender': 'M', 'Birthday': '1982-05-10', 'Age': 40, 'Alive': 'True', 'Death': 'NA', 'Child': 'F1', 'Spouse': ['F2']}
+        individualDictionary['I5'] = {'ID': 'I5', 'Name': 'Amanda /Jordan/', 'Gender': 'F', 'Birthday': '1981-03-5', 'Age': 41, 'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F2']}
+        individualDictionary['I6'] = {'ID': 'I6', 'Name': 'Nikola /Marks/', 'Gender': 'F', 'Birthday': '1999-10-7', 'Age': 23, 'Alive': 'True', 'Death': 'NA', 'Child': 'F2', 'Spouse': []}
+        individualDictionary['I7'] = {'ID': 'I7', 'Name': 'Anton /Marks/', 'Gender': 'M', 'Birthday': '1998-05-16', 'Age': 24, 'Alive': 'True', 'Death': 'NA', 'Child': 'F2', 'Spouse': []}
+
+        return individualDictionary
+
+    def generate_Family_Dictionary(self):
+        familyDictionary = {}
+        familyDictionary['F1'] = {'ID': 'F1', 'Marriage': '', 'Divorce': 'NA', 'Husband_ID': 'I3', 'Husband_Name': 'Don /Marks/', 'Wife_ID': 'I2', 'Wife_Name': 'Joyce /Earnhart/', 'Children': ['I1', 'I4']}
+        familyDictionary['F2'] = {'ID': 'F2', 'Marriage': '', 'Divorce': 'NA', 'Husband_ID': 'I4', 'Husband_Name': 'Jacob /Marks/', 'Wife_ID': 'I5', 'Wife_Name': 'Amanda /Jordan/', 'Children': ['I6', 'I7']}
+
+        return familyDictionary
+
+    # Normal marriages, anticipate true to be returned
+    def test_us19_no_marriage_to_first_cousin_1(self):
+        individualDictionary = self.generate_Individual_Dictionary()
+        familyDictionary = self.generate_Family_Dictionary()
+        self.assertTrue(us19_no_marriage_to_first_cousin(individualDictionary, familyDictionary))
+
+    # Multiple marriages to cousins, returns false
+    def test_us19_no_marriage_to_first_cousin_2(self):
+        individualDictionary = {'I1': {'ID': 'I1', 'Name': 'Joseph /Marks/', 'Gender': 'M', 'Birthday': '1985-05-14', 'Age': 37, 'Alive': 'True', 'Death': 'NA', 'Child': 'F2', 'Spouse': ['F1']}, 'I2': {'ID': 'I2', 'Name': 'Donald /Marks/', 'Gender': 'M', 'Birthday': '1949-08-15', 'Age': 73, 'Alive': 'True', 'Death': 'NA', 'Child': 'F3', 'Spouse': ['F2']}, 'I3': {'ID': 'I3', 'Name': 'Joyce /Earnhardt/', 'Gender': 'F', 'Birthday': '1951-10-24', 'Age': 71, 'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F2']}, 'I4': {'ID': 'I4', 'Name': 'Jacob /Marks/', 'Gender': 'M', 'Birthday': '1982-02-22', 'Age': 40, 'Alive': 'True', 'Death': 'NA', 'Child': 'F2', 'Spouse': ['F4']}, 'I5': {'ID': 'I5', 'Name': 'Jack /Marks/', 'Gender': 'M', 'Birthday': '1915-06-5', 'Age': 107, 'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F3']}, 'I6': {'ID': 'I6', 'Name': 'Helen /Fogler/', 'Gender': 'F', 'Birthday': '1918-09-16', 'Age': 104, 'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F3']}, 'I7': {'ID': 'I7', 'Name': 'Patricia /Marks/', 'Gender': 'F', 'Birthday': '1955-07-12', 'Age': 67, 'Alive': 'True', 'Death': 'NA', 'Child': 'F3', 'Spouse': ['F5']}, 'I8': {'ID': 'I8', 'Name': 'Brad /Stewart/', 'Gender': 'M', 'Birthday': '1951-07-6', 'Age': 71, 'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F5']}, 'I9': {'ID': 'I9', 'Name': 'Mitchell /Stewart/', 'Gender': 'M', 'Birthday': '1988-08-10', 'Age': 34, 'Alive': 'True', 'Death': 'NA', 'Child': 'F5', 'Spouse': []}, 'I10': {'ID': 'I10', 'Name': 'Alyssa /Stewart/', 'Gender': 'F', 'Birthday': '1989-05-5', 'Age': 33, 'Alive': 'True', 'Death': 'NA', 'Child': 'F5', 'Spouse': ['F4']}, 'I11': {'ID': 'I11', 'Name': 'Nikola /Stewart/', 'Gender': 'F', 'Birthday': '1989-06-8', 'Age': 33, 'Alive': 'True', 'Death': 'NA', 'Child': 'F5', 'Spouse': ['F1']}}
+        familyDictionary = {'F1': {'ID': 'F1', 'Marriage': '', 'Divorce': 'NA', 'Husband_ID': 'I1', 'Husband_Name': 'Joseph /Marks/', 'Wife_ID': 'I11', 'Wife_Name': 'Nikola /Stewart/', 'Children': []}, 'F2': {'ID': 'F2', 'Marriage': '1949-08-15', 'Divorce': 'NA', 'Husband_ID': 'I2', 'Husband_Name': 'Donald /Marks/', 'Wife_ID': 'I3', 'Wife_Name': 'Joyce /Earnhardt/', 'Children': ['I1', 'I4']}, 'F3': {'ID': 'F3', 'Marriage': '', 'Divorce': 'NA', 'Husband_ID': 'I5', 'Husband_Name': 'Jack /Marks/', 'Wife_ID': 'I6', 'Wife_Name': 'Helen /Fogler/', 'Children': ['I2', 'I7']}, 'F4': {'ID': 'F4', 'Marriage': '', 'Divorce': 'NA', 'Husband_ID': 'I4', 'Husband_Name': 'Jacob /Marks/', 'Wife_ID': 'I10', 'Wife_Name': 'Alyssa /Stewart/', 'Children': []}, 'F5': {'ID': 'F5', 'Marriage': '', 'Divorce': 'NA', 'Husband_ID': 'I8', 'Husband_Name': 'Brad /Stewart/', 'Wife_ID': 'I7', 'Wife_Name': 'Patricia /Marks/', 'Children': ['I9', 'I10', 'I11']}}
+        self.assertFalse(us19_no_marriage_to_first_cousin(individualDictionary, familyDictionary))
+
+    # Multiple marriages to multiple cousins, returns false
+    def test_us19_no_marriage_to_first_cousin_3(self):
+        individualDictionary = {'I1': {'ID': 'I1', 'Name': 'Joseph /Marks/', 'Gender': 'M', 'Birthday': '1985-05-14', 'Age': 37, 'Alive': 'True', 'Death': 'NA', 'Child': 'F3', 'Spouse': ['F1', 'F2']}, 'I2': {'ID': 'I2', 'Name': 'Donald /Marks/', 'Gender': 'M', 'Birthday': '1949-08-15', 'Age': 73, 'Alive': 'True', 'Death': 'NA', 'Child': 'F4', 'Spouse': ['F3']}, 'I3': {'ID': 'I3', 'Name': 'Joyce /Earnhardt/', 'Gender': 'F', 'Birthday': '1951-10-24', 'Age': 71, 'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F3']}, 'I4': {'ID': 'I4', 'Name': 'Jacob /Marks/', 'Gender': 'M', 'Birthday': '1982-02-22', 'Age': 40, 'Alive': 'True', 'Death': 'NA', 'Child': 'F3', 'Spouse': ['F5']}, 'I5': {'ID': 'I5', 'Name': 'Jack /Marks/', 'Gender': 'M', 'Birthday': '1915-06-5', 'Age': 107, 'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F4']}, 'I6': {'ID': 'I6', 'Name': 'Helen /Fogler/', 'Gender': 'F', 'Birthday': '1918-09-16', 'Age': 104, 'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F4']}, 'I7': {'ID': 'I7', 'Name': 'Patricia /Marks/', 'Gender': 'F', 'Birthday': '1955-07-12', 'Age': 67, 'Alive': 'True', 'Death': 'NA', 'Child': 'F4', 'Spouse': ['F6']}, 'I8': {'ID': 'I8', 'Name': 'Brad /Stewart/', 'Gender': 'M', 'Birthday': '1951-07-6', 'Age': 71, 'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F6']}, 'I9': {'ID': 'I9', 'Name': 'Mitchell /Stewart/', 'Gender': 'M', 'Birthday': '1988-08-10', 'Age': 34, 'Alive': 'True', 'Death': 'NA', 'Child': 'F6', 'Spouse': []}, 'I10': {'ID': 'I10', 'Name': 'Alyssa /Stewart/', 'Gender': 'F', 'Birthday': '1989-05-5', 'Age': 33, 'Alive': 'True', 'Death': 'NA', 'Child': 'F6', 'Spouse': ['F5']}, 'I11': {'ID': 'I11', 'Name': 'Nikola /Stewart/', 'Gender': 'F', 'Birthday': '1989-06-8', 'Age': 33, 'Alive': 'True', 'Death': 'NA', 'Child': 'F6', 'Spouse': ['F2']}, 'I12': {'ID': 'I12', 'Name': 'Alexa /Stewart/', 'Gender': 'F', 'Birthday': '1999-06-3', 'Age': 23, 'Alive': 'True', 'Death': 'NA', 'Child': 'F6', 'Spouse': ['F1']}}
+        familyDictionary = {'F1': {'ID': 'F1', 'Marriage': '2011-03-2', 'Divorce': 'NA', 'Husband_ID': 'I1', 'Husband_Name': 'Joseph /Marks/', 'Wife_ID': 'I12', 'Wife_Name': 'Alexa /Stewart/', 'Children': []}, 'F2': {'ID': 'F2', 'Marriage': '2009-02-3', 'Divorce': '2010-04-5', 'Husband_ID': 'I1', 'Husband_Name': 'Joseph /Marks/', 'Wife_ID': 'I11', 'Wife_Name': 'Nikola /Stewart/', 'Children': []}, 'F3': {'ID': 'F3', 'Marriage': '1949-08-15', 'Divorce': 'NA', 'Husband_ID': 'I2', 'Husband_Name': 'Donald /Marks/', 'Wife_ID': 'I3', 'Wife_Name': 'Joyce /Earnhardt/', 'Children': ['I1', 'I4']}, 'F4': {'ID': 'F4', 'Marriage': '', 'Divorce': 'NA', 'Husband_ID': 'I5', 'Husband_Name': 'Jack /Marks/', 'Wife_ID': 'I6', 'Wife_Name': 'Helen /Fogler/', 'Children': ['I2', 'I7']}, 'F5': {'ID': 'F5', 'Marriage': '', 'Divorce': 'NA', 'Husband_ID': 'I4', 'Husband_Name': 'Jacob /Marks/', 'Wife_ID': 'I10', 'Wife_Name': 'Alyssa /Stewart/', 'Children': []}, 'F6': {'ID': 'F6', 'Marriage': '', 'Divorce': 'NA', 'Husband_ID': 'I8', 'Husband_Name': 'Brad /Stewart/', 'Wife_ID': 'I7', 'Wife_Name': 'Patricia /Marks/', 'Children': ['I9', 'I10', 'I11', 'I12']}}
+        self.assertFalse(us19_no_marriage_to_first_cousin(individualDictionary, familyDictionary))
+
+    # child marries parent, but doesn't violate the test, returns true
+    def test_us19_no_marriage_to_first_cousin_4(self):
+        individualDictionary = {'I1': {'ID': 'I1', 'Name': 'Joseph /Marks/', 'Gender': 'M', 'Birthday': '1985-05-13', 'Age': 37, 'Alive': 'True', 'Death': 'NA', 'Child': 'F2', 'Spouse': ['F1']}, 'I2': {'ID': 'I2', 'Name': 'Donald /Marks/', 'Gender': 'M', 'Birthday': '1951-08-15', 'Age': 71, 'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F2']}, 'I3': {'ID': 'I3', 'Name': 'Joyce /Earnhardt/', 'Gender': 'F', 'Birthday': '1951-10-24', 'Age': 71, 'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F1', 'F2']}}
+        familyDictionary = {'F1': {'ID': 'F1', 'Marriage': '2007-03-4', 'Divorce': 'NA', 'Husband_ID': 'I1', 'Husband_Name': 'Joseph /Marks/', 'Wife_ID': 'I3', 'Wife_Name': 'Joyce /Earnhardt/', 'Children': []}, 'F2': {'ID': 'F2', 'Marriage': '1975-12-24', 'Divorce': '2005-03-3', 'Husband_ID': 'I2', 'Husband_Name': 'Donald /Marks/', 'Wife_ID': 'I3', 'Wife_Name': 'Joyce /Earnhardt/', 'Children': ['I1']}}
+        self.assertTrue(us19_no_marriage_to_first_cousin(individualDictionary, familyDictionary))
+
+
+
+
+class test_us20_aunts_uncles_dont_marry_nieces_nephews(unittest.TestCase):
+
+    def generate_Individual_Dictionary(self):
+        individualDictionary = {}
+        individualDictionary['I1'] = {'ID': 'I1', 'Name': 'Joseph /Marks/', 'Gender': 'M', 'Birthday': '1985-05-13', 'Age': 37, 'Alive': 'True', 'Death': 'NA', 'Child': 'F1', 'Spouse': []}
+        individualDictionary['I2'] = {'ID': 'I2', 'Name': 'Joyce /Earnhart/', 'Gender': 'F', 'Birthday': '1951-10-7', 'Age': 71, 'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F1']}
+        individualDictionary['I3'] = {'ID': 'I3', 'Name': 'Don /Marks/', 'Gender': 'M', 'Birthday': '1949-08-15', 'Age': 73, 'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F1']}
+        individualDictionary['I4'] = {'ID': 'I4', 'Name': 'Jacob /Marks/', 'Gender': 'M', 'Birthday': '1982-05-10', 'Age': 40, 'Alive': 'True', 'Death': 'NA', 'Child': 'F1', 'Spouse': ['F2']}
+        individualDictionary['I5'] = {'ID': 'I5', 'Name': 'Amanda /Jordan/', 'Gender': 'F', 'Birthday': '1981-03-5', 'Age': 41, 'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F2']}
+        individualDictionary['I6'] = {'ID': 'I6', 'Name': 'Nikola /Marks/', 'Gender': 'F', 'Birthday': '1999-10-7', 'Age': 23, 'Alive': 'True', 'Death': 'NA', 'Child': 'F2', 'Spouse': []}
+        individualDictionary['I7'] = {'ID': 'I7', 'Name': 'Anton /Marks/', 'Gender': 'M', 'Birthday': '1998-05-16', 'Age': 24, 'Alive': 'True', 'Death': 'NA', 'Child': 'F2', 'Spouse': []}
+
+        return individualDictionary
+
+    def generate_Family_Dictionary(self):
+        familyDictionary = {}
+        familyDictionary['F1'] = {'ID': 'F1', 'Marriage': '', 'Divorce': 'NA', 'Husband_ID': 'I3', 'Husband_Name': 'Don /Marks/', 'Wife_ID': 'I2', 'Wife_Name': 'Joyce /Earnhart/', 'Children': ['I1', 'I4']}
+        familyDictionary['F2'] = {'ID': 'F2', 'Marriage': '', 'Divorce': 'NA', 'Husband_ID': 'I4', 'Husband_Name': 'Jacob /Marks/', 'Wife_ID': 'I5', 'Wife_Name': 'Amanda /Jordan/', 'Children': ['I6', 'I7']}
+
+        return familyDictionary
+
+    # Standard set up with no violations - would anticipate true
+    def test_us20_aunts_uncles_dont_marry_nieces_nephews_1(self):
+        individualDictionary = self.generate_Individual_Dictionary()
+        familyDictionary = self.generate_Family_Dictionary()
+        self.assertTrue(us20_aunts_uncles_dont_marry_nieces_nephews(individualDictionary, familyDictionary))
+
+    # Niece updated to marry uncle, should return false
+    def test_us20_aunts_uncles_dont_marry_nieces_nephews_2(self):
+
+        individualDictionary = {'I1': {'ID': 'I1', 'Name': 'Joseph /Marks/', 'Gender': 'M', 'Birthday': '1985-05-13', 'Age': 37, 'Alive': 'True', 'Death': 'NA', 'Child': 'F2', 'Spouse': ['F1']}, 'I2': {'ID': 'I2', 'Name': 'Joyce /Earnhart/', 'Gender': 'F', 'Birthday': '1951-10-7', 'Age': 71, 'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F2']}, 'I3': {'ID': 'I3', 'Name': 'Don /Marks/', 'Gender': 'M', 'Birthday': '1949-08-15', 'Age': 73, 'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F2']}, 'I4': {'ID': 'I4', 'Name': 'Jacob /Marks/', 'Gender': 'M', 'Birthday': '1982-05-10', 'Age': 40, 'Alive': 'True', 'Death': 'NA', 'Child': 'F2', 'Spouse': ['F3']}, 'I5': {'ID': 'I5', 'Name': 'Amanda /Jordan/', 'Gender': 'F', 'Birthday': '1981-03-5', 'Age': 41, 'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F3']}, 'I6': {'ID': 'I6', 'Name': 'Nikola /Marks/', 'Gender': 'F', 'Birthday': '1999-10-7', 'Age': 23, 'Alive': 'True', 'Death': 'NA', 'Child': 'F3', 'Spouse': ['F1']}, 'I7': {'ID': 'I7', 'Name': 'Anton /Marks/', 'Gender': 'M', 'Birthday': '1998-05-16', 'Age': 24, 'Alive': 'True', 'Death': 'NA', 'Child': 'F3', 'Spouse': []}}
+        familyDictionary = {'F1': {'ID': 'F1', 'Marriage': '', 'Divorce': 'NA', 'Husband_ID': 'I1', 'Husband_Name': 'Joseph /Marks/', 'Wife_ID': 'I6', 'Wife_Name': 'Nikola /Marks/', 'Children': []}, 'F2': {'ID': 'F2', 'Marriage': '', 'Divorce': 'NA', 'Husband_ID': 'I3', 'Husband_Name': 'Don /Marks/', 'Wife_ID': 'I2', 'Wife_Name': 'Joyce /Earnhart/', 'Children': ['I1', 'I4']}, 'F3': {'ID': 'F3', 'Marriage': '', 'Divorce': 'NA', 'Husband_ID': 'I4', 'Husband_Name': 'Jacob /Marks/', 'Wife_ID': 'I5', 'Wife_Name': 'Amanda /Jordan/', 'Children': ['I6', 'I7']}}
+        self.assertFalse(us20_aunts_uncles_dont_marry_nieces_nephews(individualDictionary, familyDictionary))
+
+    # Uncle originally married wife, then divorced and married niece, returns false
+    def test_us20_aunts_uncles_dont_marry_nieces_nephews_3(self):
+
+        individualDictionary = {'I1': {'ID': 'I1', 'Name': 'Joseph /Marks/', 'Gender': 'M', 'Birthday': '1985-06-7', 'Age': 37, 'Alive': 'True', 'Death': 'NA', 'Child': 'F3', 'Spouse': ['F1', 'F2']}, 'I2': {'ID': 'I2', 'Name': 'Donald /Marks/', 'Gender': 'M', 'Birthday': '1949-05-4', 'Age': 73, 'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F3']}, 'I3': {'ID': 'I3', 'Name': 'Joyce /Marks/', 'Gender': 'F', 'Birthday': '1951-05-5', 'Age': 71, 'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F3']}, 'I4': {'ID': 'I4', 'Name': 'Jacob /Marks/', 'Gender': 'M', 'Birthday': '1982-03-3', 'Age': 40, 'Alive': 'True', 'Death': 'NA', 'Child': 'F3', 'Spouse': ['F4']}, 'I5': {'ID': 'I5', 'Name': 'Amanda /Jordan/', 'Gender': 'F', 'Birthday': '1981-06-3', 'Age': 41, 'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F4']}, 'I6': {'ID': 'I6', 'Name': 'Nikola /Marks/', 'Gender': 'F', 'Birthday': '2003-04-4', 'Age': 19, 'Alive': 'True', 'Death': 'NA', 'Child': 'F4', 'Spouse': ['F1']}, 'I7': {'ID': 'I7', 'Name': 'Kamela /Schweibinz/', 'Gender': 'F', 'Birthday': '1999-04-2', 'Age': 23, 'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F2']}}
+        familyDictionary = {'F1': {'ID': 'F1', 'Marriage': '2015-04-2', 'Divorce': 'NA', 'Husband_ID': 'I1', 'Husband_Name': 'Joseph /Marks/', 'Wife_ID': 'I6', 'Wife_Name': 'Nikola /Marks/', 'Children': []}, 'F2': {'ID': 'F2', 'Marriage': '2007-03-4', 'Divorce': '2015-02-4', 'Husband_ID': 'I1', 'Husband_Name': 'Joseph /Marks/', 'Wife_ID': 'I7', 'Wife_Name': 'Kamela /Schweibinz/', 'Children': []}, 'F3': {'ID': 'F3', 'Marriage': '', 'Divorce': 'NA', 'Husband_ID': 'I2', 'Husband_Name': 'Donald /Marks/', 'Wife_ID': 'I3', 'Wife_Name': 'Joyce /Marks/', 'Children': ['I1', 'I4']}, 'F4': {'ID': 'F4', 'Marriage': '', 'Divorce': 'NA', 'Husband_ID': 'I4', 'Husband_Name': 'Jacob /Marks/', 'Wife_ID': 'I5', 'Wife_Name': 'Amanda /Jordan/', 'Children': ['I6']}}
+        self.assertFalse(us20_aunts_uncles_dont_marry_nieces_nephews(individualDictionary, familyDictionary))
+
+    # Child marries parent, but doesn't violate test, should return true
+    def test_us20_aunts_uncles_dont_marry_nieces_nephews_4(self):
+        individualDictionary = {
+            'I1': {'ID': 'I1', 'Name': 'Joseph /Marks/', 'Gender': 'M', 'Birthday': '1985-05-13', 'Age': 37,
+                   'Alive': 'True', 'Death': 'NA', 'Child': 'F2', 'Spouse': ['F1']},
+            'I2': {'ID': 'I2', 'Name': 'Donald /Marks/', 'Gender': 'M', 'Birthday': '1951-08-15', 'Age': 71,
+                   'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F2']},
+            'I3': {'ID': 'I3', 'Name': 'Joyce /Earnhardt/', 'Gender': 'F', 'Birthday': '1951-10-24', 'Age': 71,
+                   'Alive': 'True', 'Death': 'NA', 'Child': 'NA', 'Spouse': ['F1', 'F2']}}
+        familyDictionary = {'F1': {'ID': 'F1', 'Marriage': '2007-03-4', 'Divorce': 'NA', 'Husband_ID': 'I1',
+                                   'Husband_Name': 'Joseph /Marks/', 'Wife_ID': 'I3', 'Wife_Name': 'Joyce /Earnhardt/',
+                                   'Children': []},
+                            'F2': {'ID': 'F2', 'Marriage': '1975-12-24', 'Divorce': '2005-03-3', 'Husband_ID': 'I2',
+                                   'Husband_Name': 'Donald /Marks/', 'Wife_ID': 'I3', 'Wife_Name': 'Joyce /Earnhardt/',
+                                   'Children': ['I1']}}
+        self.assertTrue(us20_aunts_uncles_dont_marry_nieces_nephews(individualDictionary, familyDictionary))
+
  
 class userStories21and23Test(unittest.TestCase):
   
