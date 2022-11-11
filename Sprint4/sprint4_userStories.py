57b42753ecdc448c821a62a96132c00fe67f33a8
lijp000 <lijp000@qq.com>
2022-11-11 15:28:05 -0500

    US26 US34 Added

---
+++ Sprint4/sprint4_userStories.py
@@ -0,0 +1,75 @@
+from datetime import datetime, timedelta, date
+
+
+# parse the date
+def yearsDifferenceChecker(date1, date2):
+    date1List = date1.split('-')
+    date2List = date2.split('-')
+    dateTime1 = datetime(int(date1List[0]), int(date1List[1]), int(date1List[2]))
+    dateTime2 = datetime(int(date2List[0]), int(date2List[1]), int(date2List[2]))
+    if dateTime2 > dateTime1:
+        variance = dateTime2 - dateTime1
+        return variance.days / 365.2425
+    else:
+        variance = dateTime1 - dateTime2
+        return variance.days / 365.2425
+
+def modifiedDate(date, days):
+    if days >= 0:
+        return str(datetime.strptime(date, '%Y-%m-%d') + timedelta(days=days))[:10]
+    else:
+        return str(datetime.strptime(date, '%Y-%m-%d') - timedelta(days=days))[:10]
+
+def printError(us, id, message):
+    print("ERROR US" + us + " " + id + " " + message)
+
+
+# US26 Corresponding entries
+def correspondingEntries(individualDictionary, familyDictionary):
+    flag = True
+    for indi in individualDictionary:
+        child = individualDictionary[indi]['Child']
+        spouse = individualDictionary[indi]['Spouse']
+        if (child != 'NA'):
+            if child not in familyDictionary:
+                flag = False
+                printError("26", child, "has no corresponding entry.")
+        if (spouse != []):
+            for s in spouse:
+                if s not in familyDictionary:
+                    flag = False
+                    printError("26", s, "has no corresponding entry.")
+    for fam in familyDictionary:
+        husband = familyDictionary[fam]['Husband_ID']
+        wife = familyDictionary[fam]['Wife_ID']
+        children = familyDictionary[fam]['Children']
+        if (husband != 'NA'):
+            if husband not in individualDictionary:
+                flag = False
+                printError("26", husband, "has no corresponding entry.")
+        if (wife != 'NA'):
+            if wife not in individualDictionary:
+                flag = False
+                printError("26", wife, "has no corresponding entry.")
+        if (children != []):
+            for child in children:
+                if child not in individualDictionary:
+                    flag = False
+                    printError("26", child, "has no corresponding entry.")
+    return flag
+
+
+# US34 List large age differences
+def listLargeAgeDifferences(individualDictionary, familyDictionary):
+    flag = True
+    days_in_year = 365.2425
+    for fami in familyDictionary:
+        marriage = datetime.strptime(familyDictionary[fami]['Marriage'], "%Y-%m-%d")
+        wifeAge = int((marriage - datetime.strptime(individualDictionary[familyDictionary[fami]["Wife_ID"]]['Birthday']
+                                                    , "%Y-%m-%d")).days / days_in_year)
+        husbandAge = int((marriage - datetime.strptime(individualDictionary[familyDictionary[fami]["Husband_ID"]]['Birthday']
+                                                    , "%Y-%m-%d")).days / days_in_year)
+        if (max(wifeAge, husbandAge) / min(wifeAge, husbandAge) >= 2):
+            flag = False
+            printError("34", fami, "were married when the older spouse was more than twice as old as the younger spouse.")
+    return flag
